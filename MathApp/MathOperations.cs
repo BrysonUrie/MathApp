eb3267eaee1656e075d965095dd1b4a6ca21f5bc
BrysonUrie <132849445+BrysonUrie@users.noreply.github.com>
2025-03-18 18:55:56 -0600

    fix: move subract up

--- MathApp/MathOperations.cs
+++ MathApp/MathOperations.cs
@@ -4,20 +4,25 @@ namespace MathApp
 {
     public class MathOperations
     {
+        public static int Subtract(int a, int b)
+        {
+            int diff = 0;
+            for (var i = 0; i < 1; i++)
+            {
+                diff += a;
+                Console.WriteLine("Hi");
+                diff -= b;
+            }
+
+            return diff;
+        }
+
         public static int Add(int a, int b)
         {
             int sum = 0;
             sum += a;
             sum += b;
             return sum;
-        }
-
-        public static int Subtract(int a, int b)
-        {
-            int diff = 0;
-            diff += a;
-            diff -= b;
-            return diff;
         }
     }
 }
